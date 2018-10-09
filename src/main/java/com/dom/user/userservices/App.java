3190376c2e01806dc99612821c0703fa56815741
Dominic Keefe <dom.keefe@gmail.com>
2018-10-09 14:51:09 +0100

    Initial commit

---
+++ src/main/java/com/dom/user/userservices/App.java
@@ -0,0 +1,13 @@
+package com.dom.user.userservices;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
